f6211f984c66487fda5f6ed4cac28ee788b32135
sequence <sequence@myrealm.ca>
2021-06-16 14:04:32 -0600

    Add Place, Image models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place, Image
+
+admin.site.register(Place)
+admin.site.register(Image)
--- places/models.py
+++ places/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import os
 
-print("hello!")
+from django.db import models
+from where_to_go import settings
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    description_short = models.TextField(max_length=500, blank=True)
+    description_long = models.TextField(blank=True)
+    coord_lng = models.FloatField(null=True)
+    coord_lat = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    num = 0
+    link = models.ForeignKey(Place, on_delete=models.PROTECT)
+    file = models.ImageField(upload_to=None, width_field=None)
+
+    def __str__(self):
+        self.num = self.num + 1
+        return str(self.num) + ' ' + self.link.title
