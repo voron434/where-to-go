015b8376e86daec1fd361acd9ab7364f079953e4
sequence <sequence@myrealm.ca>
2021-07-11 08:09:57 -0600

    Simplified addition of images in admin panel. Images are now shown inline in admin panel in Place and Image.

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, Image
 
 
+def balance_dims(height, width):
+    if height > 200:
+        coef = 200/height
+        width = width * coef
+        return 200, width
+    return height, width
+
+
+class ImageAdmin(admin.ModelAdmin):
+    readonly_fields = ("file_image", )
+
+    def file_image(self, obj):
+        return format_html('<img src="{url}" width="{width}" height={height}>'.
+                           format(url=obj.file.url,
+                                  width=obj.file.width,
+                                  height=obj.file.height, )
+                           )
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    readonly_fields = ("file_image", )
+
+    def file_image(self, obj):
+        img_dims = balance_dims(obj.file.height, obj.file.width)
+        print(img_dims)
+        return format_html('<img src="{url}" width="{width}" height={height}>'.
+                           format(url=obj.file.url,
+                                  width=img_dims[1],
+                                  height=img_dims[0], )
+                           )
+
+
 class PlaceAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
+    inlines = [ImageInline]
+    readonly_fields = ("id", )
 
 
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
