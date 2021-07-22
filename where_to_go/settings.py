b19899323dbcd67f3d559a14c2b85577ecdd97f4
Ilya Osipov <070809010@mail.ru>
2021-07-22 22:59:46 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'iutginteifnetingitng')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [env('ALLOWED_HOST_1')]
+ALLOWED_HOSTS = [env('ALLOWED_HOST_1'), ['ydn4xcrzohtz.pythonanywhere.com', 'localhost', '127.0.0.1']]
 
 
 # Application definition
